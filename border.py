c4bde8d865cbca78f784dcb8bde18bdeb23a3ac2
viktor <krusnik66@gmail.com>
2019-01-17 12:54:23 +0500

    border update

--- border.py
+++ border.py
@@ -2,20 +2,33 @@
 
 import sys
 import getpass
-from pexpect import pxssh
+import subprocess
 
 login = input('login: ')
-password = getpass.getpass("password: ")
+hostname = '212.49.96.88'
 
-s = pxssh.pxssh()
+class RemoteShell:
+    def __init__(self, hostname = hostname, port=514, username=login, encoding='ascii'):
+        self.hostname = hostname
+        self.port = port
+        self.username = username
+        self.encoding = encoding
+        self.rsh = ['/usr/bin/rsh', '-l', username, hostname]
 
-if not s.login ('172.16.24.146', login, password):
-    print ("SSH session failed on login.")
-    print (str(s))
-else:
-    print ("SSH session login successful")
-    s.sendline ('ls -l')
-    s.prompt()
-    print (s.before)
-    s.logout()
+    def exec_command(self, command, get_exit_code=True):
+        if get_exit_code:
+            command += '; echo $?'
+        p = subprocess.Popen(self.rsh + [ command ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
 
+        stdout, stderr = p.communicate()
+        stdout = stdout.decode(self.encoding)
+        stderr = stderr.decode(self.encoding)
+        if not get_exit_code or p.returncode != 0:
+            return stdout, stderr, p.returncode
+        return stdout, stderr
+
+sr = RemoteShell()
+for i in sr.exec_command('show version'):
+    print (str(i))
